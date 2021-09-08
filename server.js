252d103c016573fd215f407209e1a876bc305962
ravshanov1003 <ravshanov1003@gmail.com>
2021-09-08 14:27:32 +0500

    authentication

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require("express")
 
 const { listRouter } = require('./routes/listRouter')
 const { authRouter } = require('./routes/authRouter')
+const { checkUser } = require('./middleware/authMiddleware')
 
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.use('/todo', listRouter)
+app.use('/todo', checkUser, listRouter)
 app.use('/auth', authRouter)
 
 app.listen(3000, _ => console.log('server is running on port 3000'))
